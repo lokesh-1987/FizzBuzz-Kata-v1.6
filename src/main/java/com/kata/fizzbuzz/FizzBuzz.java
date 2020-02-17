3a8f2025e0b40a26e68fe507a4ffae0161c71802
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-17 12:10:39 +0530

    extracted the logic for the multiples of 3 in a method called isFizzNumber

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -5,9 +5,13 @@ class FizzBuzz {
     private static final String FIZZ = "Fizz";
 
     String getFizzBuzz(int number) {
-        if(number % 3 == 0) {
+        if(isFizzNumber(number)) {
             return FIZZ;
         }
         return String.valueOf(number);
+    }
+
+    private boolean isFizzNumber(int number) {
+        return number % 3 == 0;
     }
 }
