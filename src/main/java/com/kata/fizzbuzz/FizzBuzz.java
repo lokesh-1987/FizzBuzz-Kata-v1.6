5a53c6f5f4270dde254fd700dd8d6deae933e47b
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-17 11:30:33 +0530

    refactored to return number

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,6 @@ package com.kata.fizzbuzz;
 
 class FizzBuzz {
     int getFizzBuzz(int number) {
-        if(number == 2) {
-            return 2;
-        }
-        return 1;
+        return number;
     }
 }
