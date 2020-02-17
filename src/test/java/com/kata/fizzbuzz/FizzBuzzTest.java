02ce4cf44d34232e3089c35a2df4ef1a2f0ea542
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-17 11:32:13 +0530

    extracted fizzbuzz variable as a field and moved instatiation to setUp method

--- src/test/java/com/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/kata/fizzbuzz/FizzBuzzTest.java
@@ -2,19 +2,25 @@ package com.kata.fizzbuzz;
 
 import org.hamcrest.core.Is;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void fizzBuzzShouldReturn1For1() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assert.assertThat(fizzBuzz.getFizzBuzz(1), Is.is(1));
     }
 
     @Test
     public void fizzBuzzShouldReturn2For2() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assert.assertThat(fizzBuzz.getFizzBuzz(2), Is.is(2));
     }
 }
