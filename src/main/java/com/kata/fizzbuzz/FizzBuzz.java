73d11bfdede640f9cf525ccda69dbd5881c1cc31
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-17 12:04:58 +0530

    refactored the logic to return Fizz for multiples of 3

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -5,9 +5,7 @@ class FizzBuzz {
     private static final String FIZZ = "Fizz";
 
     String getFizzBuzz(int number) {
-        if(number == 3) {
-            return FIZZ;
-        } else if (number == 6) {
+        if(number % 3 == 0) {
             return FIZZ;
         }
         return String.valueOf(number);
